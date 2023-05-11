6b1c5b80711b304555c68fc7d7c30831d9941d37
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-05-11 15:08:40 +0900

    [これから認証機能を実装します]

---
+++ loveput/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>トップ画面</title>
+    </head>
+    <body>
+        <p>こんにちは!</p>
+    </body> 
+</html>
