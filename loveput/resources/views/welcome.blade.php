96e851ea7a2c180d6bce02c597b548ee0af68f26
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-05-14 14:50:57 +0900

    [レイアウト調整中です]

--- loveput/resources/views/welcome.blade.php
+++ loveput/resources/views/welcome.blade.php
@@ -9,92 +9,42 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
+        <!-- レイアウト -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+            body {
+                background-image: url("/images/loveput背景.jpg");
+                background-size: contain;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-position: center;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
+            .log-sign-in {
+                background-color: rgba(255,0,103,0.20);
+                color: white;
+                padding: 20px;
+                border-radius: 15px;
                 position: absolute;
-                right: 10px;
-                top: 18px;
+                transform: translate(-50%, -50%);
+                bottom: 1vh;
+                right: -10vh;
+                font-size: 20px;
             }
 
-            .content {
-                text-align: center;
+            .log-sign-in span {
+                margin-left: 10px;
             }
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+            
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        @if(!Auth::check())
+        <div class="log-sign-in">
+            <a href="{{ route('register') }}">会員登録</a>
+            <span><a href="{{ route('login') }}">ログイン</a></span>
         </div>
+        @endif
+
     </body>
 </html>
