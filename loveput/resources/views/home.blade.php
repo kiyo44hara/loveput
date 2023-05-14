3d21a2bbe60a88ae59aaeddad6fe3e9c1ac8e048
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-05-14 23:46:26 +0900

    [Top,ログイン、新規登録ページのレイアウトを完了しました]

--- loveput/resources/views/home.blade.php
+++ loveput/resources/views/home.blade.php
@@ -1,10 +1,19 @@
+<!-- トップ画面 -->
 @extends('layouts.app')
+
 <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 
 
-
     <body>
-        
+        @if(session()->has('success'))
+            <div class="alert-alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+        <div class="test">いらっしゃいませ、{{ Auth::user()->name }}さん!貴方のLoveを聞かせてください!</div>
+        <div class="select"><a href="{{ route('post') }}">はい!</a>
+            <span><a href="{{ route('posts.index') }}">いいえ!</a></span>
+        </div>
         @if(!Auth::check())
         <div class="log-sign-in">
             <a href="{{ route('register') }}">会員登録</a>
